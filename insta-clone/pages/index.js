31999b0eb1c53212320ff8355f99017b86267c06
Shashank Sharda <64969681+shashanksharda98@users.noreply.github.com>
2021-11-02 23:26:50 +0530

    stories

---
+++ insta-clone/pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Feed from '../components/Feed'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+     {/* header */}
+     <Header />
+     {/* feed */}
+     <Feed />
+     {/* modal */}
+    </div>
+  )
+}
